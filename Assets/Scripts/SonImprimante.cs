211db49d8e8772f2004a7e0de7f52e89e655d8c6
NicolasNgu <nicolas.nguyen.1802@gmail.com>
2022-12-09 17:43:09 +0100

    Added interactables and interactors, and attach point for instructions

--- Assets/Scripts/SonImprimante.cs
+++ Assets/Scripts/SonImprimante.cs
@@ -6,10 +6,12 @@ public class SonImprimante : MonoBehaviour
 {
     [SerializeField] private AudioSource _sonImprimante;
     [SerializeField] private GameObject _imprimante;
+     [SerializeField] private GameObject _consignes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _consignes.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +27,10 @@ public class SonImprimante : MonoBehaviour
         _sonImprimante.Play();
         
         
+    }
+
+      public void ConsignesApparition(){
+        _consignes.SetActive(true);
+        this.gameObject.SetActive(false);
     }
 }
