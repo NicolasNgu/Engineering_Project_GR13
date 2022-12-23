1f3f698a1dc3e4edab27075e95681e6f8df386db
NicolasNgu <nicolas.nguyen.1802@gmail.com>
2022-12-23 21:00:17 +0100

    Added appearing watch meu
    when clicking with the index on the screen of the watch, a menu will appear.
    
    Planning to add a gps app in this menu to locate a key

---
+++ Assets/Scripts/screenAppear.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenAppear : MonoBehaviour
+{
+
+    public GameObject screen;
+    public GameObject hand; 
+
+    bool isLit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLit = false;
+        screen.SetActive(false);
+        Debug.Log("start ok");
+    }
+
+    // Update is called once per frame
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == hand)
+        {
+            isLit = !isLit;
+            screen.SetActive(isLit);
+            Debug.Log("ecran touché");
+        }
+    }
+}
