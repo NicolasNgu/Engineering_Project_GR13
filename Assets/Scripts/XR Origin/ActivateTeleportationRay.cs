b594722956b540569e05eee6c4f60bd186ea9de3
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2023-02-08 22:33:45 +0100

    Teleportation

---
+++ Assets/Scripts/XR Origin/ActivateTeleportationRay.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportation;
+    public InputActionProperty leftActivate;
+
+    void Update()
+    {
+        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
