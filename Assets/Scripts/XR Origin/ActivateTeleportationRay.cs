109a2248e83c89f5061efe6025259e64d024de24
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2023-02-17 10:21:47 +0100

    First test

--- Assets/Scripts/XR Origin/ActivateTeleportationRay.cs
+++ Assets/Scripts/XR Origin/ActivateTeleportationRay.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
     public GameObject leftTeleportation;
     public InputActionProperty leftActivate;
+    public XRRayInteractor leftRay;
 
     void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        leftTeleportation.SetActive(!isLeftRayHovering && leftActivate.action.ReadValue<float>() > 0.1f);
     }
 }
