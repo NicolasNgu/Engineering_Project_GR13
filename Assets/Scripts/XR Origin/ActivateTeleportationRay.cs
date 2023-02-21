ee2840be86ff48f2a3d21788bd078d8ec6a86aca
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2023-02-21 22:59:30 +0100

    Interactable UI
    Resolve conflict with teleportation and add a first try for menu UI

--- Assets/Scripts/XR Origin/ActivateTeleportationRay.cs
+++ Assets/Scripts/XR Origin/ActivateTeleportationRay.cs
@@ -5,12 +5,22 @@ using UnityEngine.InputSystem;
 public class ActivateTeleportationRay : MonoBehaviour
 {
     public GameObject leftTeleportation;
-    public InputActionProperty leftActivate;
-    public XRRayInteractor leftRay;
+    // public InputActionProperty leftActivate;
+    // public InputActionProperty leftCancel; 
+    public GameObject leftRay;
+    public GameObject rightRay;
+
+    public GameObject menu;
 
     void Update()
     {
-        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
-        leftTeleportation.SetActive(!isLeftRayHovering && leftActivate.action.ReadValue<float>() > 0.1f);
+        // bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid); // return true if we hit something
+        // leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+
+        // Disable teleportation when menu is enable and vice-versa
+        leftTeleportation.SetActive(!menu.activeSelf);
+
+        // Same for the ray
+        leftRay.SetActive(menu.activeSelf);
     }
 }
