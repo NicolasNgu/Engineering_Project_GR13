67d3b8e4304c439de17c24c3c5596c862ea24be8
NicolasNgu <nicolas.nguyen.1802@gmail.com>
2023-03-03 18:47:24 +0100

    Added computer
    Pressing the swreen will allow the player to shut the computer down before leaving to avoid game over 1

---
+++ Assets/computerManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class computerManager : MonoBehaviour
+{
+    bool screenIsLit;
+    bool menuIsLit;
+    [SerializeField] private GameObject screen;
+    [SerializeField] private GameObject menu;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenIsLit = true;
+        menuIsLit = false;
+        screen.SetActive(screenIsLit);
+        menu.SetActive(menuIsLit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screen.SetActive(screenIsLit);
+        menu.SetActive(menuIsLit);
+    }
+
+    public void AppuiEcran()
+    {
+        menuIsLit = !menuIsLit;
+    }
+    public void DeconnecterSession()
+    {
+        screenIsLit = false;
+    }
+
+    public void ConnecterSession()
+    {
+        screenIsLit = true;
+    }
+
+
+}
