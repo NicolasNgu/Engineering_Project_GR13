d14153235d1101a2537801513ed0a1104885da02
NicolasNgu <nicolas.nguyen.1802@gmail.com>
2023-03-27 16:48:46 +0100

    added sms when clicking on the right mail
    DIIIING
    and the player is guided to the password choice

--- Assets/Scripts/guidePlayerToPass.cs
+++ Assets/Scripts/guidePlayerToPass.cs
@@ -6,11 +6,15 @@ public class guidePlayerToPass : MonoBehaviour
 {
     [SerializeField] private GameObject panneauPass;
     [SerializeField] private Compass compass;
+    [SerializeField] private GameObject sms;
+    [SerializeField] private AudioSource ding;
 
     public void panneauActivation()
     {
         panneauPass.SetActive(true);
         compass.target = panneauPass.transform;
+        sms.SetActive(true);
+        ding.Play();
         
     }
 }
