37693695781262d44020c1bf7692ce7ae88cb0be
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2023-03-27 13:40:36 +0200

    Fix printing

--- Assets/Scripts/SonImprimante.cs
+++ Assets/Scripts/SonImprimante.cs
@@ -1,35 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SonImprimante : MonoBehaviour
 {
     [SerializeField] private AudioSource _sonImprimante;
-    [SerializeField] private GameObject _imprimante;
-     [SerializeField] private GameObject _consignes;
+    [SerializeField] private GameObject _consignes;
 
-    // Start is called before the first frame update
     void Start()
     {
         _consignes.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Printing()
     {
-        
-        _sonImprimante = _imprimante.GetComponent<AudioSource>();
         _sonImprimante.Play();
-        
-        
     }
 
-      public void ConsignesApparition(){
+    public void ConsignesApparition()
+    {
         _consignes.SetActive(true);
         this.gameObject.SetActive(false);
     }
