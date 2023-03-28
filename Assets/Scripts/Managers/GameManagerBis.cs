c6029b35e43984f41125d935db60f23a6ee2b7eb
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2023-03-28 15:56:12 +0200

    Adjust Compass

--- Assets/Scripts/Managers/GameManagerBis.cs
+++ Assets/Scripts/Managers/GameManagerBis.cs
@@ -2,12 +2,38 @@ using UnityEngine;
 
 public class GameManagerBis : Singleton<GameManagerBis>
 {
-    [HideInInspector] public int numGameOver;
+    [HideInInspector] 
+    public int numGameOver;
+
+    [Header("Codes")]
+    public GameObject[] codes;
+
+    [Header("DEMO")]
+    public bool isDemo;
+    public GameObject[] codesForDemo; 
     
     public void GameOver(int number)
     {
         GameManagerBis.Instance.numGameOver = number;
         AudioManager.Instance.PlayBackgroundMusic(false);
         SceneManagerBis.Instance.ChangeScene("Game Over");
+    }
+
+    public void GGEZ()
+    {
+        bool openTheDoor = true;
+        
+        foreach (var code in codes)
+        {
+            if (code.activeSelf == false)
+            {
+                openTheDoor = false;
+            }
+        }
+
+        if (openTheDoor)
+        {
+            
+        }
     }
 }
